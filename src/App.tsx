352b1d594a841a8090e0c2eb2a5824ffca20e6ea
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-25 16:36:09 +0800

    fix: 删除app注释

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-24 23:21:25
+ * @LastEditTime: 2022-02-25 16:35:12
  * @Description: 填写简介
  */
 // import Dialog from  './dialog/Dialog';
@@ -15,23 +15,6 @@ function App() {
   return (
 	<Provider store={store}>
 		<div className="App">
-			{/* <span
-				onClick={()=>{
-					Dialog.show({
-						content: <div>hi</div>
-					})
-				}}
-			>
-				点击弹窗
-			</span>
-
-			<span
-				onClick={()=>{
-					Dialog.hide()
-				}}
-			>
-				关闭弹窗
-			</span> */}
 			<Project></Project>
 			<UseEffectDemo />
 		</div>
