301413a76b6db72f33c5ee65a959052ac3d3d27c
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-25 16:45:02 +0800

    feat:分离redux toolkit代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,19 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-25 16:35:12
+ * @LastEditTime: 2022-02-25 16:44:29
  * @Description: 填写简介
  */
 // import Dialog from  './dialog/Dialog';
 import { Provider } from 'react-redux';
 import { store } from './reduxToolkitDemo/store';
 import { Project } from './reduxToolkitDemo/view/project';
-import { UseEffectDemo } from './useEffectDemo';
 
 function App() {
   return (
 	<Provider store={store}>
 		<div className="App">
 			<Project></Project>
-			<UseEffectDemo />
 		</div>
 	</Provider>
   );
