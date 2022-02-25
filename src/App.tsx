66817ecee7d094af8db4542d16355829891fce81
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-25 16:40:08 +0800

    feat: 分离reack-hook代码

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,17 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-25 16:35:12
+ * @LastEditTime: 2022-02-25 16:38:52
  * @Description: 填写简介
  */
-// import Dialog from  './dialog/Dialog';
-import { Provider } from 'react-redux';
-import { store } from './reduxToolkitDemo/store';
-import { Project } from './reduxToolkitDemo/view/project';
+
 import { UseEffectDemo } from './useEffectDemo';
 
 function App() {
   return (
-	<Provider store={store}>
 		<div className="App">
-			<Project></Project>
 			<UseEffectDemo />
 		</div>
-	</Provider>
   );
 }
 
