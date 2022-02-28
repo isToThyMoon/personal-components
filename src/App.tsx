48f62c0e03a306f98fc5d5ca3ec0af5933d61146
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-28 13:32:52 +0800

    ci: 增加use-immerJs的commitlint prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-27 12:45:21
+ * @LastEditTime: 2022-02-28 14:03:45
  * @Description: 填写简介
  */
-import Immer from "./immer-demo/default-use";
 
 function App() {
   return (
-		<div className="App">
-			<Immer></Immer>
-		</div>
+    <div className="App">
+      <div>App</div>
+    </div>
   );
 }
-
 
 export default App;
