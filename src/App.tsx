6e95e54da49ecb6ab3211b6dd39103575937ac02
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-28 14:06:14 +0800

    ci: use-redux-toolkit 增加commitlint和prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,16 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-25 16:44:29
+ * @LastEditTime: 2022-02-28 14:06:08
  * @Description: 填写简介
  */
-// import Dialog from  './dialog/Dialog';
-import { Provider } from 'react-redux';
-import { store } from './reduxToolkitDemo/store';
-import { Project } from './reduxToolkitDemo/view/project';
 
 function App() {
   return (
-	<Provider store={store}>
-		<div className="App">
-			<Project></Project>
-		</div>
-	</Provider>
+    <div className="App">
+      <div>App</div>
+    </div>
   );
 }
-
 
 export default App;
