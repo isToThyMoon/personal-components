022a9500baabb9d8642841e4b3c04e4f868b2943
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-28 14:02:07 +0800

    ci: 增加custom-react-hook的commitlint prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,16 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-25 17:12:58
+ * @LastEditTime: 2022-02-28 14:00:49
  * @Description: 填写简介
  */
-// import Dialog from  './dialog/Dialog';
 
 function App() {
   return (
-		<div className="App">
-			
-		</div>
+    <div className="App">
+      <div>App</div>
+    </div>
   );
 }
-
 
 export default App;
