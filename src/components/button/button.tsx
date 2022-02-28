cb18e31d783e608b2815ad39f0ced0d5a4a5e1b5
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-28 16:37:24 +0800

    feat: button component initialization

---
+++ src/components/button/button.tsx
@@ -0,0 +1,13 @@
+/*
+ * @Author: 王荣
+ * @Date: 2022-02-28 16:16:36
+ * @LastEditors: 王荣
+ * @LastEditTime: 2022-02-28 16:34:51
+ * @Description: 填写简介
+ */
+
+export const Button = () => {
+  return <a></a>;
+};
+
+export default Button;
