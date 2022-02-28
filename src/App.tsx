f1b1e2eb79466b097a81d3b4ad0394ab065979ba
王巨仙 <ayrikiya@wangjuxiandeMacBook-Pro.local>
2022-02-28 13:32:52 +0800

    增加commitlint和prettier配置

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,16 @@
  * @Author: 王荣
  * @Date: 2022-02-11 14:25:28
  * @LastEditors: 王荣
- * @LastEditTime: 2022-02-28 11:39:36
+ * @LastEditTime: 2022-02-28 13:32:23
  * @Description: 填写简介
  */
 
-
 function App() {
   return (
-		<div className="App">
-		</div>
+    <div className="App">
+      <div>App</div>
+    </div>
   );
 }
-
 
 export default App;
